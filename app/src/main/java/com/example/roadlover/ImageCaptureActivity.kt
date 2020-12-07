f5371f7f289828aa43f7473a4acef0a6cae686a7
kuntal-ghosh <kuntalghosh.ewu@gmail.com>
2020-12-07 20:23:59 +0600

    border was successfully added to captured image

--- app/src/main/java/com/example/roadlover/ImageCaptureActivity.kt
+++ app/src/main/java/com/example/roadlover/ImageCaptureActivity.kt
@@ -1,11 +1,79 @@
 package com.example.roadlover
 
+import android.Manifest
+import android.content.Intent
+import android.content.pm.PackageManager
+import android.graphics.Bitmap
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.provider.MediaStore
+import android.widget.Button
+import androidx.core.app.ActivityCompat
+import androidx.core.content.ContextCompat
+import com.google.android.material.imageview.ShapeableImageView
 
 class ImageCaptureActivity : AppCompatActivity() {
+
+    private val REQUEST_PERMISSION = 100
+    private val REQUEST_IMAGE_CAPTURE = 1
+    private val REQUEST_PICK_IMAGE = 2
+
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_image_capture)
+
+        val btnCaptureImage:Button=findViewById(R.id.btnCaptureImage)
+        btnCaptureImage.setOnClickListener {
+            captureImage()
+        }
+    }
+
+    override fun onResume() {
+        super.onResume()
+        checkCameraPermission()
+    }
+
+    private fun captureImage() {
+        Intent(MediaStore.ACTION_IMAGE_CAPTURE).also { intent ->
+            intent.resolveActivity(packageManager)?.also {
+                startActivityForResult(intent, REQUEST_IMAGE_CAPTURE)
+            }
+
+        }
+    }
+
+    private fun openGallery() {
+        Intent(Intent.ACTION_GET_CONTENT).also { intent ->
+            intent.type = "image/*"
+            intent.resolveActivity(packageManager)?.also {
+                startActivityForResult(intent, REQUEST_PICK_IMAGE)
+            }
+        }
+    }
+
+    private fun checkCameraPermission() {
+        if (ContextCompat.checkSelfPermission(this, Manifest.permission.CAMERA)
+            != PackageManager.PERMISSION_GRANTED) {
+            ActivityCompat.requestPermissions(this,
+                arrayOf(Manifest.permission.CAMERA),
+                REQUEST_PERMISSION)
+        }
+    }
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+        val imageView:ShapeableImageView=findViewById(R.id.shapeableImageView)
+
+        if (resultCode == RESULT_OK) {
+            if (requestCode == REQUEST_IMAGE_CAPTURE) {
+                val bitmap = data?.extras?.get("data") as Bitmap
+                imageView.setImageBitmap(bitmap)
+            }
+            else if (requestCode == REQUEST_PICK_IMAGE) {
+                val uri = data?.getData()
+                imageView.setImageURI(uri)
+            }
+        }
     }
 }
